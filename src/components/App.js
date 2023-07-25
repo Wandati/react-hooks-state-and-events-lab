eb868f79a151fb3fbdab33c8c9c1ba7fc63968bc
Wandati <Petersimon801@gmail.com>
2023-07-25 13:02:54 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,18 @@
 import React from "react";
-import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
 
 function App() {
-
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
 
+  const [isOn, setIsOn] = useState(false);
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+  const color = isOn ? "white" : "black";
   return (
-    <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
+    <button onClick={() => handleClick} style={{ background: color }}>
+      {isOn ? "ON" : "OFF"}
+    </button>
   );
 }
 
